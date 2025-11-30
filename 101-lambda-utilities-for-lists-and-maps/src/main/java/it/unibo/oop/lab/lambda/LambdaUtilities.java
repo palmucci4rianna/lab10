b0ef505fd5969734121c37117e9afb6b89dd5d2f
Arianna Palmucci <arianna.palmucci@studio.unibo.it>
2025-11-30 15:37:08 +0100

    add 101

--- 101-lambda-utilities-for-lists-and-maps/src/main/java/it/unibo/oop/lab/lambda/LambdaUtilities.java
+++ 101-lambda-utilities-for-lists-and-maps/src/main/java/it/unibo/oop/lab/lambda/LambdaUtilities.java
@@ -2,10 +2,13 @@ package it.unibo.oop.lab.lambda;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.LinkedHashSet;
+//import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import java.util.Set;
+//import java.util.function.BiFunction;
 import java.util.function.Function;
 import java.util.function.Predicate;
 import java.util.function.Supplier;
@@ -13,8 +16,8 @@ import java.util.function.UnaryOperator;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
-import static java.util.Collections.emptyList;
-import static java.util.Collections.emptyMap;
+//import static java.util.Collections.emptyList;
+//import static java.util.Collections.emptyMap;
 
 /**
  * This class will contain four utility functions on lists and maps, of which the first one is provided as example.
@@ -64,7 +67,16 @@ public final class LambdaUtilities {
         /*
          * Suggestion: consider Optional.filter
          */
-        return emptyList();
+        final List<Optional<T>> l = new ArrayList<>();
+        list.forEach(t -> {
+            if (pre.test(t)) {
+                l.add(Optional.of(t));
+            } else {
+                l.add(Optional.empty());
+            }
+        });
+        return l;
+        //return emptyList();
     }
 
     /**
@@ -83,7 +95,19 @@ public final class LambdaUtilities {
         /*
          * Suggestion: consider Map.merge
          */
-        return emptyMap();
+        final Map<R, Set<T>> map = new HashMap<>();
+        list.forEach(t -> 
+            map.merge(
+                op.apply(t),                         // chiave calcolata
+                new LinkedHashSet<>(Set.of(t)),      // valore iniziale
+                (existing, newSet) -> {              // funzione di merge
+                    existing.addAll(newSet);
+                    return existing;
+                }
+            )
+        );
+        return map;
+        //return emptyMap();
     }
 
     /**
@@ -104,7 +128,12 @@ public final class LambdaUtilities {
          *
          * Keep in mind that a map can be iterated through its forEach method
          */
-        return emptyMap();
+        final Map<K, V> newMap = new HashMap<>();
+        map.forEach((k,v) -> {
+            newMap.put(k, v.orElseGet(def));
+        });
+        return newMap;
+        //return emptyMap();
     }
 
     /**
